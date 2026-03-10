8d94c79c16c1748efb5f3e4762ff4f1598119e3d
filippo <pasqui.filippo@franchettisalviani.net>
2026-03-10 13:33:05 +0100

    Scrittura eventi
    -definizione dei vari casi

--- ProgettoGDR/src/progettogdr/Evento.java
+++ ProgettoGDR/src/progettogdr/Evento.java
@@ -4,10 +4,41 @@
  */
 package progettogdr;
 
+import java.util.Random;
+
 /**
  *
  * @author pasqui.filippo
  */
 public class Evento {
+    private String nomeEvento;
+    Random rnd=new Random();
     
+    
+    
+    public String generaEvento(){
+        int r=rnd.nextInt(1,7);
+        switch (r){
+            case 1:
+                nomeEvento="Vecchie Glorie";//POSITIVO
+                break;
+            case 2:
+                nomeEvento="Nonno Evans";//POSITIVO
+                break;
+            case 3:
+                nomeEvento="Campo del fiume";//POSITIVO
+                break;
+            case 4:
+                nomeEvento="Sign.Dark";//NEGATIVO
+                break;
+            case 5:
+                nomeEvento="Royal Academy";//NEGATIVO
+                break;
+            case 6:
+                nomeEvento="Quinto Settore";//NEGATIVO
+            default:
+                break;
+        }
+        return nomeEvento;
+    }
 }
