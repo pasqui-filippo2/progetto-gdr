30cff2e480c4918602889b3066220401bf5c70e7
filippo <pasqui.filippo@franchettisalviani.net>
2026-03-17 13:23:30 +0100

    Metodo carica partita
    -Metodo che ti permette di caricare una partita vecchia
    -Modifica del Gestore( ora conosce inventario e personaggio)

--- ProgettoGDR/src/progettogdr/FileManager.java
+++ ProgettoGDR/src/progettogdr/FileManager.java
@@ -4,9 +4,13 @@
  */
 package progettogdr;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,10 +19,12 @@ import java.util.List;
  * @author pasqui.filippo
  */
 public  class FileManager {
-    String file="salvataggioCSV.csv";
+    static String  file="salvataggioCSV.csv";
     
     
-    public void salvaFileCSV(Personaggio p,Inventario i,GestoreGioco g){
+    public static void salvaFileCSV(Personaggio p,Inventario i,GestoreGioco g){
+        String nome=p.getNome();
+        String tecnica=p.getNomeTecnica();
         String energia = String.valueOf(p.getEnergia());
         String potenzaTiro=String.valueOf(p.getPotenzaTiro());
         String tipoElementale=p.getTipo().name();
@@ -26,8 +32,8 @@ public  class FileManager {
         String onigiri=String.valueOf(i.stampaOnigiri());
         String ravioli=String.valueOf(i.stampaRavioli());
         List<String[]> dati=Arrays.asList(
-                new String[]{"ENERGIA","POTENZA TIRO","TIPO","TURNO","ONIGIRI","RAVIOLI"},
-                new String[]{energia,potenzaTiro,tipoElementale,turno,onigiri,ravioli}
+                new String[]{"NOME","TECNICA","ENERGIA","POTENZA TIRO","TIPO","TURNO","ONIGIRI","RAVIOLI"},
+                new String[]{nome,tecnica,energia,potenzaTiro,tipoElementale,turno,onigiri,ravioli}
         );
         
         try(BufferedWriter writer=new BufferedWriter(new FileWriter(file))){
@@ -38,5 +44,59 @@ public  class FileManager {
         }catch(IOException e){
             e.printStackTrace();
         }
+    }
+    
+    public static GestoreGioco caricaFileCSV(GestoreGioco g1) throws FileNotFoundException, IOException{
+        Inventario i=new Inventario();
+        int j=0;
+        OggettoInv ogg;
+        
+        try(BufferedReader reader=new BufferedReader(new FileReader(file))){
+            String line;
+            reader.readLine();
+            while((line=reader.readLine())!=null){
+                String [] columns=line.split(",");
+                //PERSONAGGIO
+                String nome=columns[0];
+                String tecnica=columns[1];
+                int energia=Integer.parseInt(columns[2]);
+                int potenza=Integer.parseInt(columns[3]);
+                TipoElementale tipo=TipoElementale.valueOf(columns[4]);
+                
+                //TURNO GESTORE GIOCO
+                int turno=Integer.parseInt(columns[5]);
+                
+                //INVENTARIO
+                
+                int onigiri=Integer.parseInt(columns[6]);
+                int ravioli=Integer.parseInt(columns[7]);
+                while(j<=onigiri){
+                    ogg= new OggettoInv("onigiri","energia");
+                    i.addOggetto(ogg);
+                    j++;
+                }
+                while(j<=ravioli){
+                    ogg= new OggettoInv("ravioli","potenza di tiro");
+                    i.addOggetto(ogg);
+                    j++;
+                }
+                
+                switch(nome){
+                    case "Axel Blaze":
+                        AxelBlaze axel=new AxelBlaze(nome,energia,potenza,tecnica,tipo);
+                        g1=new GestoreGioco(true,turno,axel,i);
+                        break;
+                    case "Shawn Frost":
+                        ShawnFrost shawn=new ShawnFrost(nome,energia,potenza,tecnica,tipo);
+                        g1=new GestoreGioco(true,turno,shawn,i);
+                        break;
+                    case "Aitor Cazado":
+                        AitorCazado aitor=new AitorCazado(nome,energia,potenza,tecnica,tipo);
+                        g1=new GestoreGioco(true,turno,aitor,i);
+                }
+                
+            }
+        }
+        return g1;
     }
 }
